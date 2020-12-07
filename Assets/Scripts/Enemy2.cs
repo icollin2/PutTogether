8ac0ce3794241eb83abb5cea54b5f065ae6855d5
Ian Collins <icollin2@email.cpcc.edu>
2020-12-07 15:52:13 -0500

    added pathfinding, added patrol and chase states for enemy, added second enemy and separate patrol nodes

---
+++ Assets/Scripts/Enemy2.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy2 : MonoBehaviour
+{
+    Transform target;
+    NavMeshAgent agent;
+    int currentNode;
+    int previousNode;
+    public enum EnemyState
+    {
+        patrol,
+        chase
+    }
+
+    EnemyState es = EnemyState.patrol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        currentNode = Random.Range(0, GameManager.gm.nodes2.Length);
+        previousNode = currentNode;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (es)
+        {
+            case EnemyState.patrol: Patrol(); break;
+            case EnemyState.chase: Chase(); break;
+            default: break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "node2")
+        {
+            currentNode = Random.Range(0, GameManager.gm.nodes2.Length);
+            while (currentNode == previousNode)
+            {
+                currentNode = Random.Range(0, GameManager.gm.nodes2.Length);
+            }
+            previousNode = currentNode;
+        }
+        if (other.tag == "Player")
+        {
+            es = EnemyState.chase;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            es = EnemyState.patrol;
+        }
+    }
+    void Patrol()
+    {
+        agent.destination = GameManager.gm.nodes2[currentNode].position;
+    }
+
+    void Chase()
+    {
+        agent.destination = GameManager.gm.player.transform.position;
+    }
+}
